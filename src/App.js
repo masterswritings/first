a5f22c6a30bfc198105defc5d5b6811c3aa57073
srinivaschellaboina <123400056+srinivaschellaboina@users.noreply.github.com>
2024-02-29 01:32:57 -0500

    five commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hi</h1>
+        <h1>Srinivas</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
